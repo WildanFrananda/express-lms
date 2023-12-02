9137017d9911108ab8d23787c9763169b2c3bd02
Wildan Frananda <wildanfrananda@Wildans-MacBook.local>
2023-12-02 17:42:54 +0700

    feat: fix api URL, add Get Dosen API

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -41,6 +41,16 @@ class UserController {
       res.status(400).json({ error: error.message })
     }
   }
+
+  async getAll(req, res) {
+    try {
+      const users = await User.find()
+
+      res.status(200).json(users)
+    } catch (error) {
+      res.status(400).json({ error: error.message })
+    }
+  }
 }
 
 module.exports = new UserController()
